f3709502c9c12aa49e387608fafc9e607ab65199
Rameshmurugan <rameshmurugan585@gmail.com>
2025-12-26 17:02:36 +0530

    Initial project setup

---
+++ src/main/java/com/examora/backend/ExamoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.examora.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamoraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examora/backend/ExamoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examora.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
